90ec5e454e3a51fca853fac504ba378d35827d6e
Frank Rosquin <frank.rosquin@gmail.com>
2014-03-14 21:13:09 +0100

    added togglemute

--- targets/denon/denon.go
+++ targets/denon/denon.go
@@ -3,6 +3,8 @@ package denon
 import "net"
 import "fmt"
 import "time"
+import "errors"
+import "strings"
 
 import "github.com/cnf/go-claw/clog"
 import "github.com/cnf/go-claw/targets"
@@ -43,17 +45,15 @@ func setup(name string, host string, port int) *Denon {
 
 func (self *Denon) SendCommand(cmd string, args ...string) bool {
     switch cmd {
+    case "Mute":
+        return self.toggleMute()
     default:
-        clog.Debug("Looking up %s in the map", cmd)
         // clog.Debug(">>>>>>>>> %# v", self.commands)
-        if val, ok := self.commands[cmd]; ok {
-            cstr, err := val.Command(args...)
-            if err != nil {
-                return false
-            }
-            clog.Debug(">>>>>>>>> %# v", cstr)
-            return self.socketSend(cstr)
-        }
+        cstr, err := self.getCommand(cmd, args...)
+        if err != nil { return false }
+        clog.Debug(">>>>>>>>> %# v", cstr)
+        _, serr := self.socketSend(cstr)
+        if serr != nil { return false }
     }
     return false
 }
@@ -62,10 +62,22 @@ func (self *Denon) Capabilities() []string {
     return []string{}
 }
 
-func (self *Denon) socketSend(str string) bool {
+func (self *Denon) getCommand(cmd string, args ...string) (string, error) {
+    clog.Debug("Looking up %s in the map", cmd)
+    if val, ok := self.commands[cmd]; ok {
+        cstr, err := val.Command(args...)
+        if err != nil {
+            return "", err
+        }
+        return cstr, nil
+    }
+    return "", errors.New("Could not get command")
+}
+
+func (self *Denon) socketSend(str string) (cmd string, err error) {
     if self.addr == nil {
         clog.Warn("No address to sent Denon command to.")
-        return false
+        return "", errors.New("No address set")
     }
 
     tdiff := time.Since(self.last)
@@ -81,11 +93,40 @@ func (self *Denon) socketSend(str string) bool {
         if conn != nil {
             conn.Close()
         }
-        return false
+        return "", err
     }
+    conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
     clog.Debug("Sending %s to %s", str, self.name)
     fmt.Fprintf(conn, "%s\r", str)
+    reply := make([]byte, 32)
+    l, err := conn.Read(reply)
+    if err != nil { return "", err }
     conn.Close()
     self.last = time.Now()
+    return string(reply[0:l]), nil
+}
+
+func (self *Denon) toggleMute() bool {
+    r, err := self.socketSend("MU?")
+    // MuteOn
+    if err != nil { return false }
+    r = strings.TrimSpace(r)
+    clog.Debug("Found: %# v", r)
+    // _, serr := self.socketSend("MU?")
+    // if serr != nil { return false }
+    if r == "MUOFF" {
+        clog.Debug("MU OFF!")
+        cmd, err := self.getCommand("MuteOn")
+        if err != nil { return false }
+        _, serr := self.socketSend(cmd)
+        if serr != nil { return false }
+    } else if r == "MUON" {
+        clog.Debug("MU ON!")
+        cmd, err := self.getCommand("MuteOff")
+        if err != nil { return false }
+        _, serr := self.socketSend(cmd)
+        if serr != nil { return false }
+        // _, serr := self.socketSend("MUOFF")
+    }
     return true
 }
